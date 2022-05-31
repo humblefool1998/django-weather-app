cf22b8f329f2b95f14c5d89f637cce4a32f839c3
Raktim Chatterjee <chatterjeeraktim1998@gmail.com>
2022-05-31 13:58:28 +0530

    First commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\raktim\Desktop\Django-Apps\django-weather-app\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
